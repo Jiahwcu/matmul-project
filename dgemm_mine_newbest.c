3157a60193c5955f4e232418452b0c35779ec3ea
Jiahao Li <jl3838@cornell.edu>
2020-10-03 02:34:35 +0000

    update

--- dgemm_mine_newbest.c
+++ dgemm_mine_newbest.c
@@ -5,7 +5,7 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
     int i, j, k;
     for (j = 0; j < M; ++j) {
         for (k = 0; k < M; ++k) {
-	    double s = B[j*M+i];
+	    double s = B[j*M+k];
             for (i = 0; i < M; ++i) {
 		double cij = C[j*M+i];
 		cij += A[k*M+i] * s;
