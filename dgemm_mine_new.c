bffd2c2072109e8ec338fc6eab329e5bd22daf44
Jiahao Li <Jiahwcu@users.noreply.github.com>
2020-10-06 15:44:06 -0400

    update

--- dgemm_mine_new.c
+++ dgemm_mine_new.c
@@ -6,8 +6,144 @@ const char* dgemm_desc = "My awesome dgemm loop with blocks.";
 #endif
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 256)
+#define BLOCK_SIZE ((int) 128)
 #endif
+
+#include <nmmintrin.h>
+
+
+#define MM 8
+#define NN 8
+#define PP 8
+
+
+/*
+ * On the Nehalem architecture, shufpd and multiplication use the same port.
+ * 32-bit integer shuffle is a different matter.  If we want to try to make
+ * it as easy as possible for the compiler to schedule multiplies along
+ * with adds, it therefore makes sense to abuse the integer shuffle
+ * instruction.  See also
+ *   http://locklessinc.com/articles/interval_arithmetic/
+ */
+#define USE_SHUFPD
+#ifdef USE_SHUFPD
+#  define swap_sse_doubles(a) _mm_shuffle_pd(a, a, 1)
+#else
+#  define swap_sse_doubles(a) (__m128d) _mm_shuffle_epi32((__m128i) a, 0x4e)
+#endif
+
+
+/*
+ * Block matrix multiply kernel.
+ * Inputs:
+ *    A: 2-by-P matrix in column major format.
+ *    B: P-by-2 matrix in row major format.
+ * Outputs:
+ *    C: 2-by-2 matrix with element order [c11, c22, c12, c21]
+ *       (diagonals stored first, then off-diagonals)
+ */
+void kdgemm2P2(double * restrict C,
+               const double * restrict A,
+               const double * restrict B)
+{
+    // This is really implicit in using the aligned ops...
+    __builtin_assume_aligned(A, 16);
+    __builtin_assume_aligned(B, 16);
+    __builtin_assume_aligned(C, 16);
+
+    // Load diagonal and off-diagonals
+    __m128d cd = _mm_load_pd(C+0);
+    __m128d co = _mm_load_pd(C+2);
+
+    /*
+     * Do block dot product.  Each iteration adds the result of a two-by-two
+     * matrix multiply into the accumulated 2-by-2 product matrix, which is
+     * stored in the registers cd (diagonal part) and co (off-diagonal part).
+     */
+    for (int k = 0; k < PP; k += 2) {
+
+        __m128d a0 = _mm_load_pd(A+2*k+0);
+        __m128d b0 = _mm_load_pd(B+2*k+0);
+        __m128d td0 = _mm_mul_pd(a0, b0);
+        __m128d bs0 = swap_sse_doubles(b0);
+        __m128d to0 = _mm_mul_pd(a0, bs0);
+
+        __m128d a1 = _mm_load_pd(A+2*k+2);
+        __m128d b1 = _mm_load_pd(B+2*k+2);
+        __m128d td1 = _mm_mul_pd(a1, b1);
+        __m128d bs1 = swap_sse_doubles(b1);
+        __m128d to1 = _mm_mul_pd(a1, bs1);
+
+        __m128d td_sum = _mm_add_pd(td0, td1);
+        __m128d to_sum = _mm_add_pd(to0, to1);
+
+        cd = _mm_add_pd(cd, td_sum);
+        co = _mm_add_pd(co, to_sum);
+    }
+
+    // Write back sum
+    _mm_store_pd(C+0, cd);
+    _mm_store_pd(C+2, co);
+}
+
+
+/*
+ * Block matrix multiply kernel.
+ * Inputs:
+ *    A: 4-by-P matrix in column major format.
+ *    B: P-by-4 matrix in row major format.
+ * Outputs:
+ *    C: 4-by-4 matrix with element order 
+ *       [c11, c22, c12, c21,   c31, c42, c32, c41,
+ *        c13, c24, c14, c23,   c33, c44, c34, c43]
+ *       That is, C is broken into 2-by-2 sub-blocks, and is stored
+ *       in column-major order at the block level and diagonal/off-diagonal
+ *       within blocks.
+ */
+void kdgemm4P4(double * restrict C,
+               const double * restrict A,
+               const double * restrict B)
+{
+    __builtin_assume_aligned(A, 16);
+    __builtin_assume_aligned(B, 16);
+    __builtin_assume_aligned(C, 16);
+
+    kdgemm2P2(C,    A+0,   B+0);
+    kdgemm2P2(C+4,  A+2*PP, B+0);
+    kdgemm2P2(C+8,  A+0,   B+2*PP);
+    kdgemm2P2(C+12, A+2*PP, B+2*PP);
+}
+
+/*
+ * Block matrix multiply kernel.
+ * Inputs:
+ *    A: 8-by-P matrix in column major format.
+ *    B: P-by-8 matrix in row major format.
+ * Outputs:
+ *    C: 8-by-8 matrix viewed as a 2-by-2 block matrix.  Each block has
+ *       the layout from kdgemm4P4.
+ */
+void kdgemm8P8(double * restrict C,
+               const double * restrict A,
+               const double * restrict B)
+{
+    __builtin_assume_aligned(A, 16);
+    __builtin_assume_aligned(B, 16);
+    __builtin_assume_aligned(C, 16);
+
+    kdgemm4P4(C,    A+0,   B+0);
+    kdgemm4P4(C+16, A+4*PP, B+0);
+    kdgemm4P4(C+32, A+0,   B+4*PP);
+    kdgemm4P4(C+48, A+4*PP, B+4*PP);
+}
+
+void kdgemm(const double * restrict A,
+            const double * restrict B,
+            double * restrict C)
+{
+    kdgemm8P8(C, A, B);
+}
+
 
 /*
   A is M-by-K
@@ -53,6 +189,7 @@ void dgemm_small(const double * restrict A, const double * restrict B, double * 
 void basic_dgemm_square(const double * restrict A, const double * restrict B, double * restrict C)
 {
     const int M = BLOCK_SIZE;
+    const int N = SMALL_BLOCK_SIZE;
     const int n_blocks = BLOCK_SIZE/SMALL_BLOCK_SIZE;
     int bi, bj, bk;
     for (bj = 0; bj < n_blocks; ++bj) {
@@ -69,9 +206,8 @@ void basic_dgemm_square(const double * restrict A, const double * restrict B, do
     }
 }
 
-void do_copy_square_in(const int lda, const double * restrict A,  double * restrict AA){
+void do_copy_square_in(const int M, const int lda, const double * restrict A,  double * restrict AA){
     int i, j;
-    const int M = BLOCK_SIZE;
     for(j = 0; j < M; ++j){
         for(i = 0; i < M; ++i){
             AA[j*M+i] = A[j*lda+i];
@@ -79,9 +215,8 @@ void do_copy_square_in(const int lda, const double * restrict A,  double * restr
     }
 }
 
-void do_copy_square_out(const int lda, double * restrict A, const double *restrict  AA){
+void do_copy_square_out(const int M, const int lda, double * restrict A, const double *restrict  AA){
     int i, j;
-    const int M = BLOCK_SIZE;
     for(j = 0; j < M; ++j){
         for(i = 0; i < M; ++i){
              A[j*lda+i] = AA[j*M+i];
@@ -96,21 +231,13 @@ void do_block_square(const int lda,
 {
 
     const int M = BLOCK_SIZE;
-    do_copy_square_in(lda, A+i+k*lda, AA);
-    do_copy_square_in(lda, B+k+j*lda, BB);
-    do_copy_square_in(lda, C+i+j*lda, CC);
-    //memset(CC, 0, sizeof(double)*M*M);
-
-    //printf("Aij, %f\n", A[i+k*lda]);
-    //printf("AA , %f\n", AA[0]);
-    //printf("Bij, %f\n", B[k+j*lda]);
-    //printf("BB , %f\n", BB[0]);
+    do_copy_square_in(M, lda, A+i+k*lda, AA);
+    do_copy_square_in(M, lda, B+k+j*lda, BB);
+    do_copy_square_in(M, lda, C+i+j*lda, CC);
 
     basic_dgemm_square(AA, BB, CC);
 
-    do_copy_square_out(lda, C+i+j*lda, CC);
-    //printf("Cij, %f\n", C[i+j*lda]);
-    //printf("CC , %f\n", CC[0]);
+    do_copy_square_out(M, lda, C+i+j*lda, CC);
 }
 
 void do_block(const int lda,
